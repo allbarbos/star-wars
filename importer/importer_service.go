6489968b0f28ea7cba9c202beb9e8b71aa93970c
Allan Barbosa <allan.barbosa@resultadosdigitais.com.br>
2020-08-03 21:36:11 -0300

    importer pkg implementation

---
+++ importer/importer_service.go
@@ -0,0 +1,63 @@
+package importer
+
+import (
+	"log"
+	"star-wars/entity"
+	"star-wars/planet"
+	"star-wars/swapi"
+)
+
+type Service interface {
+	Process(planet entity.Planet) error
+}
+
+type service struct {
+	planetSrv planet.Service
+	swapiSrv swapi.Service
+}
+
+func NewImporter(s planet.Service, swapi swapi.Service) Service {
+	return &service{
+		planetSrv: s,
+		swapiSrv: swapi,
+	}
+}
+
+func (i service) Process(planet entity.Planet) error {
+	exists, err := i.planetSrv.Exists(planet.Name)
+
+	if err != nil {
+		log.Print(err)
+		return err
+	}
+
+	if exists {
+		log.Print("planet already registered")
+		return nil
+	}
+
+	adapter, err := i.swapiSrv.GetPlanetExternally(planet.Name)
+
+	if err != nil {
+		log.Print(err)
+		return err
+	}
+
+	// Obtém a quantidade de aparições em filmes e salva no banco de dados
+	total, err := planet.TotalAppearances(adapter.Results)
+	if err != nil {
+		log.Print(err)
+		return err
+	}
+
+	planet.TotalFilms = total
+
+	err = i.planetSrv.Save(planet)
+
+	if err != nil {
+		log.Print(err)
+		return err
+	}
+
+	return nil
+}
---
+++ importer/importer_service_test.go
@@ -0,0 +1,133 @@
+package importer
+
+import (
+	"errors"
+	"star-wars/entity"
+	"star-wars/entity/adapter"
+	"star-wars/planet/mock_planet"
+	"star-wars/swapi/mock_swapi"
+
+	"testing"
+
+	"github.com/golang/mock/gomock"
+	"github.com/stretchr/testify/assert"
+)
+
+var planetEntry entity.Planet = entity.Planet{
+	Name: "Tatooine",
+	Climate: "arid",
+	Terrain: "desert",
+	TotalFilms: 5,
+}
+
+func TestProcess( t *testing.T) {
+	ctrl := gomock.NewController(t)
+	defer ctrl.Finish()
+	swapiMock := mock_swapi.NewMockService(ctrl)
+	planetMock := mock_planet.NewMockPlanetService(ctrl)
+
+
+	adapter := adapter.Planets{
+		Results: []adapter.Planet{
+			{
+				Films: []string{"film 1", "film 2", "film 3", "film 4", "film 5"},
+			},
+		},
+	}
+
+	swapiMock.EXPECT().GetPlanetExternally("Tatooine").Return(adapter, nil)
+	planetMock.EXPECT().Exists("Tatooine").Return(false, nil)
+	planetMock.EXPECT().Save(planetEntry).Return(nil)
+
+	srv := NewImporter(planetMock, swapiMock)
+	err := srv.Process(planetEntry)
+	assert.Equal(t, nil, err)
+}
+
+// planet service: already registered
+func TestProcess_planet_registered( t *testing.T) {
+	ctrl := gomock.NewController(t)
+	defer ctrl.Finish()
+	swapiMock := mock_swapi.NewMockService(ctrl)
+	planetMock := mock_planet.NewMockPlanetService(ctrl)
+	srv := NewImporter(planetMock, swapiMock)
+
+	planetMock.EXPECT().Exists("Tatooine").Return(true, nil)
+
+	err := srv.Process(planetEntry)
+	assert.Equal(t, nil, err)
+}
+
+// planet service: save error
+func TestProcess_planet_save( t *testing.T) {
+	ctrl := gomock.NewController(t)
+	defer ctrl.Finish()
+	swapiMock := mock_swapi.NewMockService(ctrl)
+	planetMock := mock_planet.NewMockPlanetService(ctrl)
+	adapter := adapter.Planets{
+		Results: []adapter.Planet{
+			{
+				Films: []string{"film 1", "film 2", "film 3", "film 4", "film 5"},
+			},
+		},
+	}
+
+	expected := errors.New("error saving planet")
+
+	swapiMock.EXPECT().GetPlanetExternally("Tatooine").Return(adapter, nil)
+	planetMock.EXPECT().Exists("Tatooine").Return(false, nil)
+	planetMock.EXPECT().Save(planetEntry).Return(expected)
+
+	srv := NewImporter(planetMock, swapiMock)
+	err := srv.Process(planetEntry)
+	assert.Equal(t, expected, err)
+}
+
+// planet service: returns error
+func TestProcess_planet_error( t *testing.T) {
+	expected := errors.New("others errors")
+	ctrl := gomock.NewController(t)
+	defer ctrl.Finish()
+	swapiMock := mock_swapi.NewMockService(ctrl)
+	planetMock := mock_planet.NewMockPlanetService(ctrl)
+	planetMock.EXPECT().Exists("Tatooine").Return(false, expected)
+
+	srv := NewImporter(planetMock, swapiMock)
+	err := srv.Process(planetEntry)
+
+	assert.Equal(t, expected, err)
+}
+
+// swapi service: returns error
+func TestProcess_swapi_error( t *testing.T) {
+	ctrl := gomock.NewController(t)
+	defer ctrl.Finish()
+	swapiMock := mock_swapi.NewMockService(ctrl)
+	planetMock := mock_planet.NewMockPlanetService(ctrl)
+	expected := errors.New("others errors")
+
+	planetMock.EXPECT().Exists("Tatooine").Return(false, nil)
+	swapiMock.EXPECT().GetPlanetExternally("Tatooine").Return(adapter.Planets{}, expected)
+
+	srv := NewImporter(planetMock, swapiMock)
+	err := srv.Process(planetEntry)
+
+	assert.Equal(t, expected, err)
+}
+
+// calculate planet total appearances returns error
+func TestProcess_total_appearances_error( t *testing.T) {
+	ctrl := gomock.NewController(t)
+	defer ctrl.Finish()
+	swapiMock := mock_swapi.NewMockService(ctrl)
+	planetMock := mock_planet.NewMockPlanetService(ctrl)
+
+	planetMock.EXPECT().Exists("Tatooine").Return(false, nil)
+	swapiMock.EXPECT().GetPlanetExternally("Tatooine").Return(adapter.Planets{ Results: []adapter.Planet{} }, nil)
+
+	srv := NewImporter(planetMock, swapiMock)
+	err := srv.Process(planetEntry)
+
+	expected := errors.New("search did not return the planet")
+	assert.Equal(t, expected, err)
+}
