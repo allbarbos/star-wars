e22590861784c391c3503121776459a1100f8064
Allan Barbosa <allan.barbosa@resultadosdigitais.com.br>
2020-08-05 15:19:19 -0300

    main

---
+++ api/main.go
@@ -0,0 +1,33 @@
+package main
+
+import (
+	"log"
+	"net/http"
+	"os"
+	"star-wars/api/routes"
+	"time"
+
+	"github.com/joho/godotenv"
+)
+
+func main() {
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatal("Error loading .env file")
+	}
+
+	port := ":" + os.Getenv("PORT")
+	if port == ":" {
+		log.Fatal("PORT must be set")
+	}
+
+	s := &http.Server{
+		Addr:           port,
+		Handler:        routes.Config(),
+		ReadTimeout:    10 * time.Second,
+		WriteTimeout:   10 * time.Second,
+		MaxHeaderBytes: 1 << 20,
+	}
+
+	log.Panic("error at listen and serve", s.ListenAndServe())
+}
